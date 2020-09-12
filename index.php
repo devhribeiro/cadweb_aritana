896fe05200d9e335c1ed4a11df4a50ceaf58b418
Victor Hugo Ribeiro <vribeiro@fivecom.com.br>
2020-09-12 12:05:06 -0300

    ajustes

--- index.php
+++ index.php
@@ -28,30 +28,30 @@
             <h3 style="color: #333;">Cadastro de Consignado</h3>
         </div>
 
-        <form>
+        <form action="push.php" method="post">
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome Completo</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="nm_completo">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">CPF</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="000.000.000-00">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="000.000.000-00" name="cpf">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">RG</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="0.000.000 ES">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="0.000.000 ES" name="rg">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Data de Nascimento</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="00/00/000">
+                <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="00/00/000" name="dt_nasc">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Estado Civil</label>
-                <select class="form-control" id="exampleFormControlSelect1">
+                <select class="form-control" id="exampleFormControlSelect1" name="estado_civil">
                     <option>Solteiro(a)</option>
                     <option>Casado(a)</option>
                     <option>Divorciado(a)</option>
@@ -62,42 +62,42 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Mãe</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="mae">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Pai</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="pai">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="email">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="Telefone">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Endereço</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_end">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Bairro</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_bairro">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Cidade</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_cid">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Estado</label>
-                <select class="form-control" id="exampleFormControlSelect1">
+                <select class="form-control" id="exampleFormControlSelect1" name="ps_uf">
                     <option>Espirito Santo</option>
                     <option>Bahia</option>
                     <option>Minas Gerais</option>
@@ -107,12 +107,12 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Cep</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080" name="ps_cep">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Tipo de Residêcia</label>
-                <select class="form-control" id="exampleFormControlSelect1">
+                <select class="form-control" id="exampleFormControlSelect1" name="ps_resi">
                     <option>Propria</option>
                     <option>Alugada</option>
                     <option>Cedida</option>
@@ -121,7 +121,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Tempo de Residencia</label>
-                <select class="form-control" id="exampleFormControlSelect1">
+                <select class="form-control" id="exampleFormControlSelect1" name="s_temp_resi" >
                     <option>0 a 6 Meses</option>
                     <option>7 a 12 Meses</option>
                     <option>Acima de 13 Meses</option>
@@ -130,7 +130,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Possui Catão de Credito</label>
-                <select class="form-control" id="exampleFormControlSelect2">
+                <select class="form-control" id="exampleFormControlSelect2" name="cartao">
                     <option>Sim</option>
                     <option>Não</option>
                 </select>
@@ -139,23 +139,28 @@
             <h3 style="color: #333;">Dados Profissionais</h3>
 
             <div class="form-group">
+                <label for="exampleFormControlInput1">Empresa</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="prof_nm">
+            </div>
+
+            <div class="form-group">
                 <label for="exampleFormControlInput1">Endereço</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="prof_end">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Cidade</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="prof_cid">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Bairro</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="prof_bairr">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Estado</label>
-                <select class="form-control" id="exampleFormControlSelect1">
+                <select class="form-control" id="exampleFormControlSelect1" name="prof_uf">
                     <option>Espirito Santo</option>
                     <option>Bahia</option>
                     <option>Minas Gerais</option>
@@ -165,65 +170,65 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Cep</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080" name="prof_cep">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Data Admissão</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080" name="prof_admis">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Função</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080" name="prof_fun">
             </div>
 
             <h3 style="color: #333;">Referencia Pessoais</h3>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm1">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_tel1">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm2">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_tel2">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm3">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_nm3">
             </div>
 
             <div class="custom-file" style="margin-bottom: 10px; margin-top: 20px;">
-                <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                <input type="file" class="custom-file-input" id="validatedCustomFile" >
                 <label class="custom-file-label" for="validatedCustomFile">Documento de Identidade</label>
                 <!-- <div class="invalid-feedback">Erro ao carregar Identidade</div> -->
             </div>
 
             <div class="custom-file" style="margin-bottom: 10px;">
-                <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                <input type="file" class="custom-file-input" id="validatedCustomFile" >
                 <label class="custom-file-label" for="validatedCustomFile">Documento Cpf</label>
                 <!-- <div class="invalid-feedback">Erro ao carregar Identidade</div> -->
             </div>
 
             <div class="custom-file" style="margin-bottom: 10px;">
-                <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                <input type="file" class="custom-file-input" id="validatedCustomFile" >
                 <label class="custom-file-label" for="validatedCustomFile">Comprovante de Residencia</label>
                 <!-- <div class="invalid-feedback">Erro ao carregar Identidade</div> -->
             </div>
@@ -231,7 +236,7 @@
             <div style="display: flex; align-items: center; margin-bottom: 50px;">
                 <div class="form-check" style="display: flex; justify-content: center; align-items: center;">
                     <label for="exampleFormControlInput1" style="font-size: 10px;">Confirmo que as informações aqui repassadas são veridicas</label>
-                    <input class="form-check-input position-static" type="radio" name="blankRadio" id="blankRadio1" value="option1" aria-label="...">
+                    <input class="form-check-input position-static" type="radio" name="blankRadio" id="blankRadio1" value="option1" aria-label="..." name="aceite">
                 </div>
 
                 <button type="submit" class="btn btn-primary" style="width: 40%; margin-left: 20px;">Enviar</button>
---
+++ push.php
@@ -0,0 +1,51 @@
+<?php
+
+print_r($_POST);
+
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(false);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'a2plcpnl0751.prod.iad2.secureserver.net';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'cadastro@grupoaritana.com.br';                     // SMTP username
+    $mail->Password   = 'ari@@2020';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('cadastro@grupoaritana.com.br', 'Cadastro de Consignado');
+    $mail->addAddress('cadastro@grupoaritana.com.br', 'Cadastro de Consignado');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Cadastro de: ' . $_POST['nm_completo'];
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
