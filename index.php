7c349a6f478286b9404fb2ef394d192f7ed007c3
Victor Hugo Ribeiro <vribeiro@fivecom.com.br>
2020-09-12 10:44:52 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Hello, world!</h1>
+
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nome Completo</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">CPF</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="000.000.000-00">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">RG</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="0.000.000 ES">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Data de Nascimento</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="00/00/000">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Estado Civil</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Solteiro(a)</option>
+                    <option>Casado(a)</option>
+                    <option>Divorciado(a)</option>
+                    <option>Viuvo(a)</option>
+                    <option>União Estavel</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Mãe</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Pai</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Telefone</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Endereço</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Bairro</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Cidade</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Estado</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Espirito Santo</option>
+                    <option>Bahia</option>
+                    <option>Minas Gerais</option>
+                    <option>Rio de Janeiro</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Cep</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect2">Tipo de Residêcia</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Propria</option>
+                    <option>Alugada</option>
+                    <option>Cedida</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect2">Tempo de Residencia</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>0 a 6 Meses</option>
+                    <option>7 a 12 Meses</option>
+                    <option>Acima de 13 Meses</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect2">Possui Catão de Credito</label>
+                <select class="form-control" id="exampleFormControlSelect2">
+                    <option>Sim</option>
+                    <option>Não</option>
+                </select>
+            </div>
+
+            <h3>Dados Profissionais</h3>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Endereço</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Cidade</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Bairro</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Estado</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Espirito Santo</option>
+                    <option>Bahia</option>
+                    <option>Minas Gerais</option>
+                    <option>Rio de Janeiro</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Cep</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Data Admissão</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Função</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080">
+            </div>
+
+            <h3>Referencia Pessoais</h3>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nome</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Telefone</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+            </div>
+            
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nome</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Telefone</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nome</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Telefone</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX">
+            </div>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
