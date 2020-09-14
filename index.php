04dbf740eb03d182d040de28e912243fefccdb51
Victor Hugo Ribeiro <vribeiro@fivecom.com.br>
2020-09-14 09:46:02 -0300

    ajustes

--- index.php
+++ index.php
@@ -31,27 +31,27 @@
         <form action="push.php" method="post" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome Completo - Não utilize acentuação!</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="nm_completo" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="nm_completo" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">CPF</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="000.000.000-00" name="cpf" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="000.000.000-00" name="cpf" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">RG</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="0.000.000 ES" name="rg" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="0.000.000 ES" name="rg" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Data de Nascimento</label>
-                <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="00/00/000" name="dt_nasc" required>
+                <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="00/00/000" name="dt_nasc" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Estado Civil</label>
-                <select class="form-control" id="exampleFormControlSelect1" name="estado_civil" required>
+                <select class="form-control" id="exampleFormControlSelect1" name="estado_civil" >
                     <option>Solteiro(a)</option>
                     <option>Casado(a)</option>
                     <option>Divorciado(a)</option>
@@ -62,7 +62,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Mãe</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="mae" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="mae" >
             </div>
 
             <div class="form-group">
@@ -77,27 +77,27 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="tel" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="tel" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Endereço</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_end" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_end" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Bairro</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_bairro" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_bairro" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Cidade</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_cid" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ps_cid" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Estado</label>
-                <select class="form-control" id="exampleFormControlSelect1" name="ps_uf" required>
+                <select class="form-control" id="exampleFormControlSelect1" name="ps_uf" >
                     <option>Espirito Santo</option>
                     <option>Bahia</option>
                     <option>Minas Gerais</option>
@@ -107,12 +107,12 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Cep</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080" name="ps_cep" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="EX: 29.162-080" name="ps_cep" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Tipo de Residêcia</label>
-                <select class="form-control" id="exampleFormControlSelect1" name="ps_resi" required>
+                <select class="form-control" id="exampleFormControlSelect1" name="ps_resi" >
                     <option>Propria</option>
                     <option>Alugada</option>
                     <option>Cedida</option>
@@ -121,7 +121,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Tempo de Residencia</label>
-                <select class="form-control" id="exampleFormControlSelect1" name="ps_temp_resi" required>
+                <select class="form-control" id="exampleFormControlSelect1" name="ps_temp_resi" >
                     <option>0 a 6 Meses</option>
                     <option>7 a 12 Meses</option>
                     <option>Acima de 13 Meses</option>
@@ -130,7 +130,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Possui Catão de Credito</label>
-                <select class="form-control" id="exampleFormControlSelect2" name="cartao" required>
+                <select class="form-control" id="exampleFormControlSelect2" name="cartao" >
                     <option>Sim</option>
                     <option>Não</option>
                 </select>
@@ -140,7 +140,7 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Empresa</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="prof_nm" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="prof_nm" >
             </div>
 
             <div class="form-group">
@@ -187,36 +187,36 @@
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm1" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm1" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_tel1" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_tel1" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm2" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm2" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_tel2" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_tel2" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Nome</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm3" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" name="ref_nm3" >
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Telefone</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_nm3" required>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="(27) XXXXX-XXXX" name="ref_nm3" >
             </div>
 
             <div class="custom-file" style="margin-bottom: 10px; margin-top: 20px;">
-                <input type="file" class="custom-file-input" id="validatedCustomFile" name="arq_rg" required>
+                <input type="file" class="custom-file-input" id="validatedCustomFile" name="arq_rg" >
                 <label class="custom-file-label" for="validatedCustomFile">Documento de Identidade</label>
                 <!-- <div class="invalid-feedback">Erro ao carregar Identidade</div> -->
             </div>
@@ -228,7 +228,7 @@
             </div>
 
             <div class="custom-file" style="margin-bottom: 10px;">
-                <input type="file" class="custom-file-input" id="validatedCustomFile" name="arq_compro" required>
+                <input type="file" class="custom-file-input" id="validatedCustomFile" name="arq_compro" >
                 <label class="custom-file-label" for="validatedCustomFile">Comprovante de Residencia</label>
                 <!-- <div class="invalid-feedback">Erro ao carregar Identidade</div> -->
             </div>
@@ -236,7 +236,7 @@
             <div style="display: flex; align-items: center; margin-bottom: 50px;">
                 <div class="form-check" style="display: flex; justify-content: center; align-items: center;">
                     <label for="exampleFormControlInput1" style="font-size: 10px;">Confirmo que as informações aqui repassadas são veridicas</label>
-                    <input class="form-check-input position-static" type="radio" name="blankRadio" id="blankRadio1" value="option1" aria-label="..." name="aceite" required>
+                    <input class="form-check-input position-static" type="radio" name="blankRadio" id="blankRadio1" value="option1" aria-label="..." name="aceite" >
                 </div>
 
                 <button type="submit" class="btn btn-primary" style="width: 40%; margin-left: 20px;">Enviar</button>
